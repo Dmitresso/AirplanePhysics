9d3fb2f182cf1908224485609fd4a2a1bff49d6a
Dmitresso <dmitresso@gmail.com>
2021-08-23 13:13:58 +0700

    Building the Airplane Inputs/Keyboard Input - C# Properties

--- Assets/AirplanePhysics/Code/Scripts/Input/BaseAirplane_Input.cs
+++ Assets/AirplanePhysics/Code/Scripts/Input/BaseAirplane_Input.cs
@@ -4,14 +4,29 @@ namespace WheelApps {
     public class BaseAirplane_Input : MonoBehaviour {
 
         #region Variables
-        public float pitch = 0f;
-        public float roll = 0f;
-        public float yaw = 0f;
-        public float throttle = 0f;
-        public int flaps = 0;
-        public float brake = 0f;
+        protected float pitch = 0f;
+        protected float roll = 0f;
+        protected float yaw = 0f;
+        protected float throttle = 0f;
+        protected int flaps = 0;
+        protected float brake = 0f;
         #endregion
 
+        #region Properties
+        public float Pitch => pitch;
+
+        public float Roll => roll;
+
+        public float Yaw => yaw;
+
+        public float Throttle => throttle;
+
+        public int Flaps => flaps;
+
+        public float Brake => brake;
+        #endregion
+        
+        
         #region Builtin Metods
         private void Start() { }
 
