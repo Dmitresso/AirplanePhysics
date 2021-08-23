1f9f54a380e45066a1ae986d44570c94130ce5b0
Dmitresso <dmitresso@gmail.com>
2021-08-23 15:33:23 +0700

    Building the Airplane Inputs/Xbox Input - Updating the Editor

--- Assets/AirplanePhysics/Code/Scripts/Input/Editor/BaseAirplaneInput_Editor.cs
+++ Assets/AirplanePhysics/Code/Scripts/Input/Editor/BaseAirplaneInput_Editor.cs
@@ -32,7 +32,6 @@ namespace WheelApps {
             
             Repaint();
         }
-
         #endregion
     }
 }
--- Assets/AirplanePhysics/Code/Scripts/Input/Editor/XboxAirplaneInput_Editor.cs
+++ Assets/AirplanePhysics/Code/Scripts/Input/Editor/XboxAirplaneInput_Editor.cs
@@ -1,5 +1,37 @@
-﻿namespace WheelApps {
-    public class XboxAirplaneInput_Editor {
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace WheelApps {
+    [CustomEditor(typeof(XboxAirplane_Input))]
+    public class XboxAirplaneInput_Editor : Editor {
+        #region Variables
+        private BaseAirplane_Input targetInput;
+        #endregion
+
         
+        #region Builtin Methods
+        private void OnEnable() {
+            targetInput = (XboxAirplane_Input) target;
+        }
+
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            
+            var debugInfo = string.Empty;
+            debugInfo += "Pitch: " + targetInput.Pitch + "\n";
+            debugInfo += "Roll: " + targetInput.Roll + "\n";
+            debugInfo += "Yaw: " + targetInput.Yaw + "\n";
+            debugInfo += "Throttle: " + targetInput.Throttle + "\n";
+            debugInfo += "Brake: " + targetInput.Brake + "\n";
+            debugInfo += "Flaps: " + targetInput.Flaps + "\n";
+
+            GUILayout.Space(20);
+            EditorGUILayout.TextArea(debugInfo, GUILayout.Height(100));
+            GUILayout.Space(20);
+            
+            Repaint();
+        }
+        #endregion
     }
 }
