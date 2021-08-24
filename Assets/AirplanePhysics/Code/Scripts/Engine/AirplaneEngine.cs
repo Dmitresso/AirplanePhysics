d638a8eaf12656b1b3f6e5a4fb6eb6c8a84d39c8
Dmitresso <dmitresso@gmail.com>
2021-08-24 21:00:29 +0700

    Building the Airplane Engine Component/Calculating Force with Animation Curves

--- Assets/AirplanePhysics/Code/Scripts/Engine/AirplaneEngine.cs
+++ Assets/AirplanePhysics/Code/Scripts/Engine/AirplaneEngine.cs
@@ -5,6 +5,8 @@ namespace WheelApps {
         #region Variables
         public float maxForce = 5000f;
         public float maxRPM = 2550f;
+        
+        public AnimationCurve powerCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
         #endregion
         
         #region Builtin Methods
@@ -13,8 +15,11 @@ namespace WheelApps {
         #region Custom Methods
         public Vector3 CalculateForce(float throttle) {
             var finalThrottle = Mathf.Clamp01(throttle);
+            finalThrottle = powerCurve.Evaluate(finalThrottle);
+            
             var finalPower = finalThrottle * maxForce;
             var finalForce = finalPower * transform.forward;
+            
             return finalForce;
         }
         #endregion
