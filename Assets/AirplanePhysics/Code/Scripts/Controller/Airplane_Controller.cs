bb52c0174178411a40a75c5379dabf34fa4536ae
Dmitresso <dmitresso@gmail.com>
2021-08-24 11:13:41 +0700

    Creating the Airplane Controller Script/Creating the Airplane Controller Class

--- Assets/AirplanePhysics/Code/Scripts/Controller/Airplane_Controller.cs
+++ Assets/AirplanePhysics/Code/Scripts/Controller/Airplane_Controller.cs
@@ -1,8 +1,13 @@
-﻿namespace WheelApps {
+﻿using UnityEngine;
+
+namespace WheelApps {
     public class Airplane_Controller : BaseRigidbody_Controller {
         #region Variables
-
-        
+        [Header("Base Airplane Properties")]
+        public XboxAirplane_Input input;
+        [Tooltip("Weight in LBS")]
+        public float airplaneWeight = 800f;
+        public Transform centerOfGravity;
 
         #endregion
         
@@ -10,10 +15,32 @@
         
         #region Custom Methods
         protected override void HandlePhysics() {
+            HandleEngines();
+            HandleAerodynamics();
+            HandleSteering();
+            HandleBrakes();
+            HandleAltitude();
+        }
+
+        private void HandleEngines() {
             
         }
-        
 
+        private void HandleAerodynamics() {
+            
+        }
+
+        private void HandleSteering() {
+            
+        }
+
+        private void HandleBrakes() {
+            
+        }
+
+        private void HandleAltitude() {
+            
+        }
         #endregion
     }
 }
