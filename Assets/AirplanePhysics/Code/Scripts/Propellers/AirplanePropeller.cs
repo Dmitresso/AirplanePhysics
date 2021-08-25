2744c23f6b3e0e43335caf86f6b8e2a96844e520
Dmitresso <dmitresso@gmail.com>
2021-08-25 18:26:51 +0700

    Animating the Propeller/Material Swapping

--- Assets/AirplanePhysics/Code/Scripts/Propellers/AirplanePropeller.cs
+++ Assets/AirplanePhysics/Code/Scripts/Propellers/AirplanePropeller.cs
@@ -7,8 +7,15 @@ namespace WheelApps {
 
         [Header("Propeller Properties")]
         public float minQuadRPMs = 300f;
+        public float minTextureSwap = 600f;
         public GameObject mainProp;
         public GameObject blurredProp;
+        
+        
+        [Header("Material Properties")]
+        public Material blurredPropMat;
+        public Texture2D blurLevel1; 
+        public Texture2D blurLevel2; 
         #endregion
 
 
@@ -36,6 +43,11 @@ namespace WheelApps {
             if (currentRPM > minQuadRPMs) {
                 blurredProp.gameObject.SetActive(true);
                 mainProp.gameObject.SetActive(false);
+
+                if (blurredPropMat && blurLevel1 && blurLevel2) {
+                    if (currentRPM > minTextureSwap) blurredPropMat.SetTexture("_MainTex", blurLevel2);
+                    else blurredPropMat.SetTexture("_MainTex", blurLevel1);
+                }
             }
             else {
                 blurredProp.gameObject.SetActive(false);
