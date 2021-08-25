986e2977ecee360506a88f29aa95f69fec38d207
Dmitresso <dmitresso@gmail.com>
2021-08-25 18:14:17 +0700

    Animating the Propeller/Blending the Propellers

--- Assets/AirplanePhysics/Code/Scripts/Propellers/AirplanePropeller.cs
+++ Assets/AirplanePhysics/Code/Scripts/Propellers/AirplanePropeller.cs
@@ -1,13 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace WheelApps {
     public class AirplanePropeller : MonoBehaviour {
         #region Variables
+
+        [Header("Propeller Properties")]
+        public float minQuadRPMs = 300f;
+        public GameObject mainProp;
+        public GameObject blurredProp;
         #endregion
 
 
 
         #region Builtin Methods
+
+        private void Start() {
+            if (mainProp && blurredProp) HandleSwapping(0f);            
+        }
+
         #endregion
 
 
@@ -17,7 +28,21 @@ namespace WheelApps {
             // degrees per second
             var dps = currentRPM * 360f / 60f * Time.deltaTime;
             transform.Rotate(Vector3.forward, dps);
+
+            if (mainProp && blurredProp) HandleSwapping(currentRPM);
         }
+
+        private void HandleSwapping(float currentRPM) {
+            if (currentRPM > minQuadRPMs) {
+                blurredProp.gameObject.SetActive(true);
+                mainProp.gameObject.SetActive(false);
+            }
+            else {
+                blurredProp.gameObject.SetActive(false);
+                mainProp.gameObject.SetActive(true);
+            }
+        }
+        
         #endregion
     }
 }
