d60bcfa71b8d0aa7389868cca68d2a911560b6cf
Dmitresso <dmitresso@gmail.com>
2021-08-27 16:58:22 +0700

    Animating the Control Surfaces/Coding the Control Surface Script

--- Assets/AirplanePhysics/Code/Scripts/ControlSurfaces/AirplaneControlSurface.cs
+++ Assets/AirplanePhysics/Code/Scripts/ControlSurfaces/AirplaneControlSurface.cs
@@ -1,9 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace WheelApps {
+    public enum ControlSurfaceType {
+        Rudder,
+        Elevator,
+        Flap,
+        Aileron
+    }
+    
     public class AirplaneControlSurface : MonoBehaviour {
         #region Variables
 
+        [Header("Control Surfaces Properties")]
+        public ControlSurfaceType type = ControlSurfaceType.Rudder;
+        public float maxAngle = 30f;
+        public Vector3 axis = Vector3.right;
+        public Transform controlSurfaceGraphic;
+        public float smoothSpeed;
+
+        private float targetAngle;
         #endregion
 
         
@@ -13,13 +29,25 @@ namespace WheelApps {
             
         }
 
+        private void Update() {
+            if (!controlSurfaceGraphic) return;
+            var finalAngleAxis = axis * targetAngle;
+            controlSurfaceGraphic.localRotation = Quaternion.Slerp(controlSurfaceGraphic.localRotation, Quaternion.Euler(finalAngleAxis), smoothSpeed * Time.deltaTime);
+        }
+
         #endregion
         
         
 
         #region Custom Methods
         public void HandleControlSurface(BaseAirplaneInput input) {
-            
+            var inputValue = type switch {
+                ControlSurfaceType.Rudder => input.Yaw,
+                ControlSurfaceType.Elevator => input.Pitch,
+                ControlSurfaceType.Flap => input.Flaps,
+                ControlSurfaceType.Aileron => input.Roll
+            };
+            targetAngle = maxAngle * inputValue;
         }
         #endregion
     }
