4680a7b80efa875522be249302553c5c11b0a256
Dmitresso <dmitresso@gmail.com>
2021-08-27 09:30:11 +0700

    Building the Airplane Camera/Stub in Scripts

---
+++ Assets/AirplanePhysics/Code/Scripts/Cameras/AirplaneCamera.cs
@@ -0,0 +1,23 @@
+﻿namespace WheelApps {
+    public class AirplaneCamera : BasicFollowCamera {
+        #region Variables
+
+        #endregion
+
+
+        
+        #region Builtin Methods
+        private void FixedUpdate() {
+            HandleCamera();
+        }
+        #endregion
+        
+        
+
+        #region Custom Methods
+        protected override void HandleCamera() {
+            base.HandleCamera();
+        }
+        #endregion
+    }
+}
---
+++ Assets/AirplanePhysics/Code/Scripts/Cameras/BasicFollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace WheelApps {
+    public class BasicFollowCamera : MonoBehaviour {
+        #region Variables
+
+        #endregion
+
+
+        
+        #region Builtin Methods
+        private void FixedUpdate() {
+            HandleCamera();
+        }
+        #endregion
+        
+        
+
+        #region Custom Methods
+
+        protected virtual void HandleCamera() {
+            
+        }
+        #endregion
+    }
+}
