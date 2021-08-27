d801901a2573c7b75bef569421f8bcc938d03603
Dmitresso <dmitresso@gmail.com>
2021-08-27 19:19:33 +0700

    Ground Effect/Stubbing in the Script

---
+++ Assets/AirplanePhysics/Code/Scripts/Features/AirplaneGroundEffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace WheelApps {
+    public class AirplaneGroundEffect : MonoBehaviour {
+        #region Variables
+        private Rigidbody rb;
+
+
+        #endregion
+
+
+        #region Builtin Methods
+
+        private void Start() {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate() {
+            if (!rb) return;
+            HandleGroundEffect();
+        }
+
+        #endregion
+
+
+        #region Custom Methods
+        protected virtual void HandleGroundEffect() {
+            
+        }
+
+
+        #endregion
+    }
+}
