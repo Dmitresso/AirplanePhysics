bd777d22ad8498be0d99224aa67c67f83bf24e49
Dmitresso <dmitresso@gmail.com>
2021-08-27 11:02:28 +0700

    Building the Airplane Camera/Create a Basic Follow Camera

--- Assets/AirplanePhysics/Code/Scripts/Cameras/BasicFollowCamera.cs
+++ Assets/AirplanePhysics/Code/Scripts/Cameras/BasicFollowCamera.cs
@@ -3,14 +3,20 @@
 namespace WheelApps {
     public class BasicFollowCamera : MonoBehaviour {
         #region Variables
+        [Header("Basic Follow Camera Properties")]
+        public Transform target;
+        public float distance = 10f;
+        public float height = 6f;
+        public float smoothSpeed = 0.5f;
 
+        private Vector3 smoothVelocity;
         #endregion
 
 
         
         #region Builtin Methods
         private void FixedUpdate() {
-            HandleCamera();
+            if(target) HandleCamera();
         }
         #endregion
         
@@ -19,7 +25,10 @@ namespace WheelApps {
         #region Custom Methods
 
         protected virtual void HandleCamera() {
+            var targetPosition = target.position + - target.forward * distance + Vector3.up * height;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref smoothVelocity, smoothSpeed);
             
+            transform.LookAt(target);
         }
         #endregion
     }
