82c62be91ed368767ff905122135e1e094a9d105
Dmitresso <dmitresso@gmail.com>
2021-08-27 18:14:52 +0700

    Completing the Wheels/Braking

--- Assets/AirplanePhysics/Code/Scripts/Wheels/AirplaneWheel.cs
+++ Assets/AirplanePhysics/Code/Scripts/Wheels/AirplaneWheel.cs
@@ -7,10 +7,13 @@ namespace WheelApps {
 
         [Header("Wheel Properties")]
         public Transform wheelGraphic;
-        
+        public bool isBraking;
+        public float brakePower = 5f;
+
         private WheelCollider collider;
         private Vector3 worldPosition;
         private Quaternion worldRotation;
+        private float finalBrakeForce;
         #endregion
 
 
@@ -33,9 +36,19 @@ namespace WheelApps {
         public void HandleWheel(BaseAirplaneInput input) {
             if (!collider) return;
             collider.GetWorldPose(out worldPosition, out worldRotation);
-            if (!wheelGraphic) return;
-            wheelGraphic.position = worldPosition;
-            wheelGraphic.rotation = worldRotation;
+            if (wheelGraphic) {
+                wheelGraphic.position = worldPosition;
+                wheelGraphic.rotation = worldRotation;                
+            }
+
+            if (input.Brake > 0.1f) {
+                finalBrakeForce = Mathf.Lerp(finalBrakeForce, input.Brake * brakePower, Time.deltaTime);
+                collider.brakeTorque = finalBrakeForce;
+            }
+            else {
+                collider.brakeTorque = 0f;
+                collider.motorTorque = 0.000000001f;
+            }
         }
         #endregion
     }
