e0ac68d4f026b73ae8434ab8d38c1f71a43ba7ba
Dmitresso <dmitresso@gmail.com>
2021-08-29 16:49:19 +0700

    Airplane Audio/Setup & Stubbing in the Script

---
+++ Assets/AirplanePhysics/Code/Scripts/Audio/AirplaneAudio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace WheelApps {
+    public class AirplaneAudio : MonoBehaviour {
+        #region Variables
+        [Header("Airplane Audio Properties")]
+        public BaseAirplaneInput input;
+        public AudioSource idleSrc;
+        public AudioSource fullThrottleSrc;
+
+        #endregion
+
+
+
+        #region Builtin Methods
+        private void Update() {
+            if (!input) return;
+            HandleAudio();    
+        }
+        #endregion
+
+
+
+        #region Custom Methods
+        protected virtual void HandleAudio() {
+            
+        }
+        #endregion
+    }
+}
