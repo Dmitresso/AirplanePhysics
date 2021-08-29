963b9a68537bde135de41df11382fbc1478f46d6
Dmitresso <dmitresso@gmail.com>
2021-08-29 10:56:46 +0700

    Airplane Editors/Saving Presets with Scriptable Objects/Part 1

---
+++ Assets/AirplanePhysics/Code/Scripts/Data/AirplanePreset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace WheelApps {
+    [CreateAssetMenu(menuName = "WheelApps/Create Airplane Preset")]
+    public class AirplanePreset : ScriptableObject {
+        #region Controller Properties
+        [Header("Controller Properties")]
+        public Vector3 comPosition;
+        public float airplaneWeight;
+        
+
+        #endregion
+    }
+}
