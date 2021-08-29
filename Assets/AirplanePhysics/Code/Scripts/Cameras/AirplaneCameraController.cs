3d254903731d9e9e87fe4bc48579c8515f0b3b36
Dmitresso <dmitresso@gmail.com>
2021-08-29 18:15:33 +0700

    Camera Switching/Managing the Audio Listeners

--- Assets/AirplanePhysics/Code/Scripts/Cameras/AirplaneCameraController.cs
+++ Assets/AirplanePhysics/Code/Scripts/Cameras/AirplaneCameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace WheelApps {
@@ -6,20 +7,27 @@ namespace WheelApps {
         #region Variables
         [Header("Camera Controller Properties")]
         public BaseAirplaneInput input;
+        public int startCameraIndex;
         public List<Camera> cameras = new List<Camera>();
 
-        private int cameraIndex = 0;
+        private int cameraIndex;
         #endregion
 
 
 
         #region Builtin Methods
+        private void Start() {
+            if (startCameraIndex < 0 || startCameraIndex >= cameras.Count) return;
+            DisableAllCameras();
+            cameras[startCameraIndex].enabled = true;
+            cameras[cameraIndex].GetComponent<AudioListener>().enabled = true;
+        }
 
+        
         private void Update() {
             if (!input) return;
             if (input.CameraSwitch) SwitchCamera();
         }
-
         #endregion
 
 
@@ -31,11 +39,16 @@ namespace WheelApps {
             cameraIndex++;
             if (cameraIndex >= cameras.Count) cameraIndex = 0;
             cameras[cameraIndex].enabled = true;
+            cameras[cameraIndex].GetComponent<AudioListener>().enabled = true;
         }
 
+        
         private void DisableAllCameras() {
             if (cameras.Count <= 0) return;
-            foreach (var cam in cameras) cam.enabled = false;
+            foreach (var cam in cameras) {
+                cam.enabled = false;
+                cam.GetComponent<AudioListener>().enabled = false;
+            }
         }
         #endregion
     }
