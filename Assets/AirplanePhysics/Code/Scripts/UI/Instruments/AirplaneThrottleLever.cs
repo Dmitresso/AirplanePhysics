c718532970e0ca080e091a67b08cb0d7907d6290
Dmitresso <dmitresso@gmail.com>
2021-08-30 18:14:47 +0700

    Airplane UI/Creating the Throttle Lever - Code

---
+++ Assets/AirplanePhysics/Code/Scripts/UI/Instruments/AirplaneThrottleLever.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+namespace WheelApps {
+    public class AirplaneThrottleLever : MonoBehaviour, IAirplaneUI {
+        #region Variables
+        [Header("Throttle Lever Properties")]
+        public BaseAirplaneInput input;
+        public RectTransform parentRect;
+        public RectTransform handleRect;
+        public float handleSpeed = 2f;
+        #endregion
+
+
+
+        #region Interface Metods
+        public void HandleAirplaneUI() {
+            if (!input || !parentRect || !handleRect) return;
+            var height = parentRect.rect.height;
+            var targetHandlePosition = new Vector2(0f, height * input.StickyThrottle);
+            handleRect.anchoredPosition = Vector2.Lerp(handleRect.anchoredPosition, targetHandlePosition, handleSpeed * Time.deltaTime);
+
+        }
+        #endregion
+    }
+}
