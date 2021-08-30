715ef5fe0c96af4e807c89e9128a294e381ef7f3
Dmitresso <dmitresso@gmail.com>
2021-08-30 17:26:22 +0700

    Airplane UI/Creating the Fuel Gauge Code

---
+++ Assets/AirplanePhysics/Code/Scripts/UI/Instruments/AirplaneFuelGauge.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace WheelApps {
+    public class AirplaneFuelGauge : MonoBehaviour, IAirplaneUI {
+        #region Variables
+        [Header("Fuel Gauge Properties")]
+        public AirplaneFuel fuel;
+        public RectTransform pointer;
+        public Vector2 minMaxRotation = new Vector2(-90f, 90f);
+        #endregion
+
+
+        
+        #region Builtin Methods
+        public void HandleAirplaneUI() {
+            if (fuel && pointer) {
+                var targetRotation = Mathf.Lerp(minMaxRotation.x, minMaxRotation.y, fuel.NormalizedFuel);
+                pointer.rotation = Quaternion.Euler(0f, 0f, - targetRotation);
+            }
+        }
+        #endregion
+        
+
+    }
+}
