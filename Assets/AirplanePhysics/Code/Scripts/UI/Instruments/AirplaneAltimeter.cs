af0b9a58d421307102cdd6958d60e520df1e15bc
Dmitresso <dmitresso@gmail.com>
2021-08-30 12:52:15 +0700

    Airplane UI/Creating the Altimeter Coding/Part 2

--- Assets/AirplanePhysics/Code/Scripts/UI/Instruments/AirplaneAltimeter.cs
+++ Assets/AirplanePhysics/Code/Scripts/UI/Instruments/AirplaneAltimeter.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace WheelApps {
     public class AirplaneAltimeter : MonoBehaviour, IAirplaneUI {
         #region Variables
-        
+        [Header("Altimeter Properties")]
+        public AirplaneController airplane;
+
+        public RectTransform hundredsPointer;
+        public RectTransform thousandsPointer;
         #endregion
 
 
@@ -19,7 +22,25 @@ namespace WheelApps {
 
         #region Interface Methods
         public void HandleAirplaneUI() {
+            if (!airplane) return;
+            var currentAltitude = airplane.CurrentMSL;
+            var currentThousands = currentAltitude * 0.001f;
+            currentThousands = Mathf.Clamp(currentThousands, 0f, 10f);
+
+            var currentHundreds = currentAltitude - Mathf.Floor(currentThousands) * 1000f;
+            currentHundreds = Mathf.Clamp(currentHundreds, 0f, 1000f);
             
+            if (thousandsPointer) {
+                var normalizedThousands = Mathf.InverseLerp(0f, 10f, currentThousands);
+                var thousandsRotation = normalizedThousands * 360f;
+                thousandsPointer.rotation = Quaternion.Euler(0f, 0f, - thousandsRotation);                
+            }
+
+            if (hundredsPointer) {
+                var normalizedHundreds = Mathf.InverseLerp(0f, 1000f, currentHundreds);
+                var hundredsRotation = normalizedHundreds * 360f;
+                hundredsPointer.rotation = Quaternion.Euler(0f, 0f, - hundredsRotation);
+            }
         }
         #endregion
 
