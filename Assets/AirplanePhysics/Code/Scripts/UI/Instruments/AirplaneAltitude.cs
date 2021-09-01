2de4613c227df4bb0ef2e6e0371142e65abb0c59
Dmitresso <dmitresso@gmail.com>
2021-09-01 17:47:56 +0700

    Airplane UI/Creating the Attitude Indicator - Code/Part 2

--- Assets/AirplanePhysics/Code/Scripts/UI/Instruments/AirplaneAltitude.cs
+++ Assets/AirplanePhysics/Code/Scripts/UI/Instruments/AirplaneAltitude.cs
@@ -17,7 +17,15 @@ namespace WheelApps {
             if (!airplane) return;
             var bankAngle = Vector3.Dot(airplane.transform.right, Vector3.up) * Mathf.Rad2Deg;
             var pitchAngle = Vector3.Dot(airplane.transform.forward, Vector3.up) * Mathf.Rad2Deg;
+            if (!bgRect) return;
+            var bankRot = Quaternion.Euler(0f, 0f, bankAngle);
+            bgRect.transform.rotation = bankRot;
 
+            var targetPosition = new Vector3(0f, pitchAngle, 0f);
+            bgRect.anchoredPosition = targetPosition;
+
+            if (!arrowRect) return;
+            arrowRect.transform.rotation = bankRot;
         }
         #endregion
 
