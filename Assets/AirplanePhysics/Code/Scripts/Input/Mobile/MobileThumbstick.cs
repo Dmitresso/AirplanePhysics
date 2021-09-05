27ef557a3559cf57a9059b5f2229c5a1d46518f1
Dmitresso <dmitresso@gmail.com>
2021-09-05 17:49:48 +0700

    Mobile Controls/Coding the Thumbstick/Part 4

--- Assets/AirplanePhysics/Code/Scripts/Input/Mobile/MobileThumbstick.cs
+++ Assets/AirplanePhysics/Code/Scripts/Input/Mobile/MobileThumbstick.cs
@@ -16,8 +16,14 @@ namespace WheelApps {
 
 
 
-        #region Builtin Methods
+        #region Properties
+        public float HorizontalAxis => finalDelta.x;
+        public float VerticalAxis => finalDelta.y;
+        #endregion
+        
 
+        
+        #region Builtin Methods
         private void Start() {
             bounds = GetComponent<RectTransform>();
         }
@@ -38,8 +44,7 @@ namespace WheelApps {
             else HandleTouches();
         }
 
-
-
+        
         private void HandleMouse() {
             if (Input.GetMouseButton(0)) {
                 if (isTouching) HandleDragging();
@@ -56,15 +61,23 @@ namespace WheelApps {
         }
 
 
-        private void HandleDragging() {
+        protected virtual void HandleDragging() {
             var targetPosition = Vector2.zero;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(bounds, Input.mousePosition, null, out targetPosition);
             knob.anchoredPosition = Vector2.Lerp(knob.anchoredPosition, targetPosition, knobSpeed * Time.deltaTime);
+
+            var knobAnchoredPosition = knob.anchoredPosition;
+            var boundsRect = bounds.rect;
+            var xDelta = knobAnchoredPosition.x / (boundsRect.width * 0.5f);
+            var yDelta = knobAnchoredPosition.y / (boundsRect.height * 0.5f);
+            finalDelta = new Vector2(xDelta, yDelta);
+            finalDelta = Vector2.ClampMagnitude(finalDelta, 1f);
         }
 
 
         private void ResetKnob() {
             knob.anchoredPosition = Vector2.Lerp(knob.anchoredPosition, Vector2.zero, knobSpeed * Time.deltaTime);
+            finalDelta = Vector2.zero;
         }
         #endregion
     }
